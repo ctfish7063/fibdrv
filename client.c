62031db256a561f37807ba7a086e11cddd4ea9fd
ctfish <a0936281757@gmail.com>
2023-05-16 17:29:34 +0800

    Apply big number data structure to client
    This commit apply big number data structure to client, the printf
    method  is modified. The client first allocates a buffer
    with a pre-calculated size, then copy a string from kernel space to the
    buffer, finally print the copied string.

--- client.c
+++ client.c
@@ -5,13 +5,16 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define DIVISOR 1000000
+#define LOG10PHI 208987
+#define LOG10SQRT5 349485
+
 #define FIB_DEV "/dev/fibonacci"
 
 int main()
 {
     long long sz;
 
-    char buf[1];
     char write_buf[] = "testing writing";
     int offset = 100; /* TODO: try test something bigger than the limit */
 
@@ -27,21 +30,29 @@ int main()
     }
 
     for (int i = 0; i <= offset; i++) {
+        size_t list_size = (i * LOG10PHI - LOG10SQRT5) / DIVISOR + 2;
+        char *buf = malloc(sizeof(char) * list_size);
+        memset(buf, '\0', sizeof(char) * list_size);
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
+        sz = read(fd, buf, sizeof(char) * list_size);
         printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               " at offset %d, returned the sequence %s.\n",
+               i, buf);
+        free(buf);
+        buf = NULL;
     }
 
     for (int i = offset; i >= 0; i--) {
+        size_t list_size = (i * LOG10PHI - LOG10SQRT5) / DIVISOR + 2;
+        char *buf = malloc(sizeof(char) * list_size);
+        memset(buf, '\0', sizeof(char) * list_size);
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
+        sz = read(fd, buf, sizeof(char) * list_size);
         printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               " at offset %d, returned the sequence %s.\n",
+               i, buf);
+        free(buf);
+        buf = NULL;
     }
 
     close(fd);
