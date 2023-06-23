d06e5023e364b53c1327299a6ea88a7363d1473b
ctfish <a0936281757@gmail.com>
2023-06-24 03:15:59 +0800

    Improve measurement
    To improve measurement of program, a loop is added to make sure the
    codes to be run are in cache.

--- test.c
+++ test.c
@@ -9,7 +9,7 @@
 #include <time.h>
 #include <unistd.h>
 
-#define limit 10000
+#define limit 1000000
 #define DIVISOR 100000
 #define LOG2PHI 69424
 #define LOG2SQRT5 116096
@@ -25,33 +25,9 @@ long long getnanosec()
     return ts.tv_sec * 1000000000L + ts.tv_nsec;
 }
 
-char *bn_2_string(uint64_t *head, int head_size, uint64_t n)
-{
-    // log10(fib(n)) = nlog10(phi) - log10(5)/2
-    double logfib = n * 0.20898764025 - 0.34948500216;
-    size_t size = n > 1 ? (size_t) logfib + 2 : 2;
-    char *res = malloc(sizeof(char) * size);
-    res[--size] = '\0';
-    if (n < 3) {
-        res[0] = !!head[0] + '0';
-        return res;
-    }
-    for (int i = size; --i >= 0;) {
-        uint128_t tmp = 0;
-        for (int j = head_size; --j >= 0;) {
-            tmp <<= 64;
-            tmp |= head[j];
-            head[j] = tmp / 10;
-            tmp %= 10;
-        }
-        res[i] = tmp + '0';
-    }
-    return res;
-}
-
 int main(int argc, char *argv[])
 {
-    int offset = limit; /* TODO: try test something bigger than the limit */
+    int offset = 10000; /* TODO: try test something bigger than the limit */
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
         perror("Failed to open character device");
@@ -63,23 +39,37 @@ int main(int argc, char *argv[])
     }
     if (argc > 2) {
         offset = atoi(argv[2]);
+        if (offset < 0) {
+            offset = limit;
+        }
     }
-    for (uint64_t i = 0; i <= offset; i++) {
-        long long kt, ut, st;
-        uint64_t n = i;
+    for (int i = 0; i < 100; i++) {
+        long long sz;
         size_t list_size =
-            n > 1 ? (n * LOG2PHI - LOG2SQRT5) / DIVISOR / 64 + 1 : 1;
+            i > 1 ? ((double) i * 0.69424191363 - 1.16096404744) / 64 + 1 : 1;
         uint64_t *buf = malloc(sizeof(uint64_t) * list_size);
         memset(buf, 0, sizeof(uint64_t) * list_size);
-        st = getnanosec();
-        lseek(fd, n, SEEK_SET);
-        kt = read(fd, buf, sizeof(uint64_t) * list_size);
-        ut = getnanosec() - st;
-        char *res = bn_2_string(buf, list_size, n);
-        printf("%lu %lld %lld %lld\n", n, kt, ut, ut - kt);
-        free(res);
+        lseek(fd, i, SEEK_SET);
+        sz = read(fd, buf, sizeof(uint64_t) * list_size);
         free(buf);
+        buf = NULL;
     }
+    // for (uint64_t i = 0; i <= offset; i++) {
+    long long kt, ut, st;
+    // uint64_t n = i;
+    uint64_t n = offset;
+    size_t list_size =
+        n > 1 ? ((double) n * 0.69424191363 - 1.16096404744) / 64 + 1 : 1;
+    uint64_t *buf = malloc(sizeof(uint64_t) * list_size);
+    memset(buf, 0, sizeof(uint64_t) * list_size);
+    st = getnanosec();
+    lseek(fd, n, SEEK_SET);
+    kt = read(fd, buf, sizeof(uint64_t) * list_size);
+    ut = getnanosec() - st;
+    printf("%lu %lld %lld %lld\n", n, kt, ut, ut - kt);
+    free(buf);
+    buf = NULL;
+    // }
 
     close(fd);
     return 0;
